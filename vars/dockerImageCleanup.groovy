42e26bfaea5a6a166b93830973121188968a90ce
unknown <nathanxa@gmail.com>
2023-04-15 22:40:24 +0530

    Docker ecr function updated

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,6 +1,13 @@
+// def call(String project, String ImageTag, String hubUser){
+//     sh """
+//       docker rmi tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+//       docker rmi tag ${hubUser}/${project} ${hubUser}/${project}:latest
+//     """ 
+// }
+
 def call(String project, String ImageTag, String hubUser){
     sh """
-      docker rmi tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-      docker rmi tag ${hubUser}/${project} ${hubUser}/${project}:latest
+      docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+      
     """ 
 }

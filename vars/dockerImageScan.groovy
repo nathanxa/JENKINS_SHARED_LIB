1f41eda1af3245d32c5e936115d9dcc4a4a5fbe5
unknown <nathanxa@gmail.com>
2023-04-15 22:37:54 +0530

    Docker ecr function updated

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,6 +1,13 @@
+// def call(String project, String ImageTag, String hubUser){
+//     sh """
+//       trivy image ${hubUser}/${project}:latest > scan.txt
+//       cat scan.txt
+//     """ 
+// }
+
 def call(String project, String ImageTag, String hubUser){
-    sh """
-      trivy image ${hubUser}/${project}:latest > scan.txt
-      cat scan.txt
-    """ 
+  sh """
+  trivy image ${aws_account_id}.dkr.ect.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+   cat scan.txt
+   """ 
 }

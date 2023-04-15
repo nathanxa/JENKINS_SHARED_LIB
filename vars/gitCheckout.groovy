0bf3c8d1f84e72ba5f370f5f218cb748e6bc85d0
unknown <nathanxa@gmail.com>
2023-04-15 11:59:50 +0530

    groovy error fix

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,7 +2,7 @@ def call(Map stageParams) {
 
     checkout([
         $class: 'GitSCM',
-        branches: [[name: stageParams.branch ]],
+        branches: [[name:  stageParams.branch ]],
         userRemoteConfigs: [[ url: stageParams.url ]]
     ])
-}
+  }
